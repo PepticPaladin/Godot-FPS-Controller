04bc7c0bc5063e9a60dbc0e619523986241f0bad
PepticPaladin <46826983+PepticPaladin@users.noreply.github.com>
2019-05-26 22:14:19 -0600

    added options to show mouse during gameplay

--- Fps Controller Tutorial/Main.gd
+++ Fps Controller Tutorial/Main.gd
@@ -1,19 +1,19 @@
 extends Spatial
 
-
+var invOpen : bool # is the inventory open?
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	invOpen = false
 
 func _process(delta):
-	var invOpen = false
 	
 	if (Input.is_action_just_pressed("toggle_inventory")):
 		invOpen = !invOpen
 		
-	if (Input.is_action_pressed("hold_inventory")):
+	if (Input.is_action_just_pressed("hold_inventory")):
 		invOpen = true
-	else:
+	elif (Input.is_action_just_released("hold_inventory")):
 		invOpen = false
 		
 	if (invOpen):
@@ -28,7 +28,9 @@ func _process(delta):
 		
 	if (Input.is_action_just_pressed("restart")):
 		get_tree().reload_current_scene()
-		
+	
+	
+	
 func _input(event):
 	if event is InputEventMouseButton:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
